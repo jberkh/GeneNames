9bc8123f4e130c6b1c061c65660b41fa8ae67a48
Jari Berkhout <73251322+jberkh@users.noreply.github.com>
2024-08-21 15:54:12 +0200

    GeneNames 0.1.2: Added force_unique functionality, allowing no duplicates in output.

--- R/StandardizeGeneNames.R
+++ R/StandardizeGeneNames.R
@@ -10,7 +10,17 @@
 #' @examples
 #' StandardizeGeneNames(genelist)
 
-StandardizeGeneNames <- function(genes, organism = c("mmu")) {
+StandardizeGeneNames <- function(
+  genes,
+  organism = c("mmu"),
+  force_unique = FALSE) {
+
+  # Input validation
+  if (force_unique & any(duplicated(genes))) {
+    stop("Input genes may not contain duplicates when force_unique == TRUE")
+  }
+
+  # Select conversion table
   if (organism == "mmu") {
     data = GeneNames::mmu
   }
@@ -26,8 +36,15 @@ StandardizeGeneNames <- function(genes, organism = c("mmu")) {
   df$converted <- df$input
   df$converted[!already_symbol] <- mapvalues(
     df$input[!already_symbol],
-    convtab$synonym[!is.na(convtab$synonym)],
-    convtab$symbol[!is.na(convtab$synonym)])
+    convtab$synonym,
+    convtab$symbol)
+
+  # Remove duplicates after first pass conversion
+  if (force_unique) {
+    duplicates = !already_symbol &
+      duplicated(df$converted, allItems = T)
+    df$converted[duplicates] = df$input[duplicates]
+  }
 
   # Again keep all input genes that are already symbols
   already_symbol = df$converted %in% data$all_symbols
@@ -41,8 +58,16 @@ StandardizeGeneNames <- function(genes, organism = c("mmu")) {
   convtab = convtab[!duplicated(convtab$synonym, allItems = T),]
   df$converted[!already_symbol] <- mapvalues(
     df$input[!already_symbol],
-    convtab$synonym[!is.na(convtab$synonym)],
-    convtab$symbol[!is.na(convtab$synonym)])
+    convtab$synonym,
+    convtab$symbol)
+
+  # Remove duplicates after second pass conversion
+  if (force_unique) {
+    duplicates = !already_symbol &
+      duplicated(df$converted, allItems = T)
+    df$converted[duplicates] = df$input[duplicates]
+
+  }
 
   pct_cnv <- round(sum(df$input != df$converted)/nrow(df) * 100, 2)
   print("Percentages of genes changed: " + pct_cnv + "%")
